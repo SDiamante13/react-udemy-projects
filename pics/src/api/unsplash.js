9aff8d57b437f738a967023c5a01e393b268e6a4
Andreea Barbaselu and Steven Diamante <pair+andreea barbaselu+steven_a_diamante@hcsc.net>
2020-10-01 23:19:08 -0500

    Finish up pics project.

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const accessKey =  process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 export default axios.create({
-    baseURL: "https://api.unsplash.com",
-    headers: {
-        Authorization: `Client-ID ${accessKey}`
-    }
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: `Client-ID ${accessKey}`,
+  },
 });
